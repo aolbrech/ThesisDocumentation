b5eb81906da955b1256ae488b229d95aa7923dc8
Annik <annik.olbrechts@cern.ch>
2014-08-01 16:53:58 +0200

    testing PythonScript

---
+++ PushToGitBranch.py
@@ -0,0 +1,20 @@
+#! python
+
+import time
+import os
+
+#This python file will perform the necessary comments to push all the information in this directory to the correct branch on the GitHub directory!
+
+OneFile = raw_input(" All information in this directory will be pushed to the DocumentationFile branch on GitHub \n Press (n) if only a specific file should be pushed and not the entire directory. \n (y) will push the entire directory.  ")
+CommentInfo = raw_input(" Which information should be given to the commit ?  ")
+
+print "Input obtained !"
+GitHubComment = ""
+if OneFile == "y" or OneFile == "Y":
+    os.system("git add *")
+    print "Obtained input :",str(CommentInfo.split())
+    for ii in range(len(CommentInfo.strip())):
+        GitHubComment += CommentInfo[ii]+""
+        print "GitHubComment for counter ",ii,"is equal to:",GitHubComment
+    os.system("git commit -m '"+str(GitHubComment)+"'")
+
