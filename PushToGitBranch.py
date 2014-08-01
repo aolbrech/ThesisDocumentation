c0615417a3c3ae95aab4a35b0cd611fb5f156112
Annik <annik.olbrechts@cern.ch>
2014-08-01 17:14:40 +0200

    Optimizing Python Script

--- PushToGitBranch.py
+++ PushToGitBranch.py
@@ -1,11 +1,15 @@
 #! python
 
-import time
+import sys
 import os
 
 #This python file will perform the necessary comments to push all the information in this directory to the correct branch on the GitHub directory!
 
-OneFile = raw_input(" All information in this directory will be pushed to the DocumentationFile branch on GitHub \n Press (n) if only a specific file should be pushed and not the entire directory. \n (y) will push the entire directory.  ")
+print " ************************************** \nOverview of the git status in this directory!: \n"
+os.system('git status')
+print " \n **************************************"
+
+OneFile = raw_input("All information in this directory will be pushed to the DocumentationFile branch on GitHub \n * Press (n) if only a specific file should be pushed and not the entire directory. \n * Pressing (y) will push the entire directory. \n * In case no add or commit should be done, press (e(xit)) ")
 
 if OneFile == "y" or OneFile == "Y":
     print "In 'y' option "
@@ -13,7 +17,9 @@ if OneFile == "y" or OneFile == "Y":
 elif OneFile == "n" or OneFile == "N":
     print "In 'n' option "
     WhichFile = raw_input("--> Which file should be added?  ")
-    os.system("git add "+WhichFile)   
+    os.system("git add "+WhichFile)
+elif OneFile == "e" or OneFile == "E" or OneFile == "exit" or OneFile == "EXIT" or OneFile == "Exit":
+    sys.exit(0)
 
 GitHubComment = ""
 if OneFile == "y" or OneFile == "Y" or OneFile == "n" or OneFile == "N":
@@ -22,7 +28,7 @@ if OneFile == "y" or OneFile == "Y" or OneFile == "n" or OneFile == "N":
         GitHubComment += CommentInfo[ii]+""
     os.system("git commit -m '"+str(GitHubComment)+"'")
     print "-------------------------------------------------------------"
-    PerformPush = raw_input("Should the commit be pushed to 'anomCouplings' remote and 'DocumentationFile' branch or should this be done only later?  (y/n)")
+    PerformPush = raw_input("Should the commit be pushed to 'anomCouplings' remote and 'DocumentationFile' branch or should this be done only later?  (y/n)  ")
     if PerformPush == "y" or PerformPush == "Y":
         os.system("git push anomCouplings DocumentationFile")
     elif PerformPush == "n" or PerformPush == "N":
